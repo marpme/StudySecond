5802ac6d5ee17c0a494e6d1d7451dd9aa45ffd29
marpme <cryzar95@googlemail.com>
2016-10-24 22:24:57 +0200

    rework

---
+++ AAD_ExerciseOne/src/MainTest.java
@@ -0,0 +1,36 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by kyon on 24.10.16.
+ */
+public class MainTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void euclidRec() throws Exception {
+
+    }
+
+    @Test
+    public void euclidIt() throws Exception {
+
+    }
+
+    @Test
+    public void euclidRestRec() throws Exception {
+
+    }
+
+    @Test
+    public void euclidRestIt() throws Exception {
+
+    }
+
+}
