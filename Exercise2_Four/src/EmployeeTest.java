0cb794cfe6821fbf67d83661525c4fdf0ab41db5
Marvin Piekarek <marvinpiekarek@Marvins-MacBook-Air.local>
2016-11-04 13:05:39 +0100

    packaging

--- Exercise2_Four/src/EmployeeTest.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by marvinpiekarek on 01.11.16.
- */
-public class EmployeeTest {
-    public static void main(String[] args) {
-        Employee e = new Employee("Marvin", "Piekarek", 10000);
-        System.out.println(e);
-    }
-}
